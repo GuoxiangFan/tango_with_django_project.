6c31ce172878d18247e7ca96545281415985350d
Guoxiang Fan <2583405F@student.gla.ac.uk>
2021-07-27 22:57:56 +0100

    chapter4

--- rango/urls.py
+++ rango/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from rango import views
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
